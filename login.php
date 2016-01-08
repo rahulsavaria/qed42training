2ee65e8e36878522ce9e8371a27b368978d6aa61
System Administrator <root@Prashants-MacBook-Air-2.local>
2016-01-08 23:46:22 +0530

    First commit for login

---
+++ login.php
@@ -0,0 +1,26 @@
+<?php
+$username = $_POST["user"];
+$password = $_POST["pass"];
+
+#Connect with mysql-server
+$conn = mysqli_connect('localhost','root','mysql360') /*or die("FAILED TO CONNECT WITH mysql-server".mysqli_connect_error())*/;
+
+#Verify input username and password from database
+mysqli_select_db($conn,"qed42") or die('oops database selection problem ! --> '.mysqli_error());
+
+$res=mysqli_query($conn,"SELECT * FROM user WHERE u_name='$username'");
+if(!$res)
+	{ echo "<script type="text/javascript">alert("Invalid username")';'</script>";} 
+else 
+	{
+		$row=mysqli_fetch_array($res);
+		if($row['u_password']==$password)
+ 			{
+  				
+  				header("Location: records.php");
+ 			}
+ 		else
+ 		{ echo "<script>alert('Invalid password')';'</script>"; }
+        
+ 	}
+?> 	
