cd20808800c7f15ad7f89826cd79cc41a4d03034
rahul <rahulsavaria@gmail.com>
2016-01-11 12:04:54 +0530

    made changes in registration.php

--- html/registration.php
+++ html/registration.php
@@ -15,10 +15,7 @@ for($i=0 ;$i<strlen($u_password);$i++)
 
 
 
-$servername  = "localhost";
-$username = "root";
-$password = "root";
-$dbname = "drupal";
+include "authentication.php";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
