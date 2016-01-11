584a53d27ea5c505ae15e89a9c0735853f055516
rahul <rahulsavaria@gmail.com>
2016-01-11 18:33:34 +0530

    session added

--- html/forget.php
+++ html/forget.php
@@ -1,6 +1,7 @@
+
 <html>
 	<body>
-		<form action ="../forget.php" method="post">
+		<form action ="../forget_data.php" method="post">
 			<h1> enter your email address</h1>
 			<input type="text" name="emailid" >
 			<input type="submit" value="send">
