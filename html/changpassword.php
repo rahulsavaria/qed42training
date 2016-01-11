c546a22fad41e0746276a5efef3d2fa91d42015f
Anand <anandtoshniwal@hotmail.com>
2016-01-11 18:37:00 +0530

    final

--- html/changpassword.php
+++ html/changpassword.php
@@ -1,10 +1,18 @@
 <html>
+  <head>
+    <center><h1>Change Password</h1></center>
+    <link rel="stylesheet" type="text/css" href="../css/changepassword.css">
+    </head>
+  <body>
 	<form action="updatepassword.php" method="post">
-		enter your username<input type="text " name="userna">
-		enter your password mailed<input type="text" name="pass" id="mpassword">
-		enter your new  password<input type="text" name="pass1" id="password">
-		reconfirm password<input type="text" name="pass2" id ="repassword">
-		<input type="submit" value ="change">
+		<center><div>
+    <input type="text " name="userna"  class="box" placeholder="enter your username">
+		<input type="text" name="pass" id="mpassword" class="box" placeholder="enter your password mailed">
+		<input type="text" name="pass1" id="password" class="box" placeholder="enter your new  password">
+		<input type="text" name="pass2" id ="repassword" class="box" placeholder="reconfirm password">
+		<input type="submit" value ="change" id="btn">
+  </div></center>
 	</form>
+</body>
 </hmtl>
 
